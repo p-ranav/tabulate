fcba10180251c70fb7fc39ff13bd0091b5275bd2
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-19 20:40:32 -0600

    Updated table to include <cell>

--- include/tables/table.hpp
+++ include/tables/table.hpp
@@ -1,24 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <tables/cell.hpp>
 
 namespace tables {
-
-class Cell {
-public:
-  explicit Cell(const std::string& contents) : contents_(contents) {}
-
-  void print() {
-    std::cout << contents_;
-  }
-
-  size_t get_width() {
-    return contents_.size();
-  }
-  
-private:
-  std::string contents_;
-};
 
 class Row {
 public:
