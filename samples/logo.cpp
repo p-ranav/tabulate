37c23ac12038facfd3628d19be0f724f9048c079
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-22 08:47:24 -0600

    Added logo sample

---
+++ samples/logo.cpp
@@ -0,0 +1,20 @@
+#include <tabulate/table.hpp>
+using namespace tabulate;
+
+int main() {
+
+  Table logo;
+
+  // Add rows
+  logo.add_row({"tabulate for Modern C++"});
+
+  // Format the table
+  logo.format()
+    .font_style({FontStyle::bold})
+    .font_align(FontAlign::center)
+    .color(Color::white)
+    .border_color(Color::yellow);
+
+  // Print the table
+  std::cout << logo << std::endl;
+}
