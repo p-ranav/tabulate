83c319a18d808358f9fde0cdf662fc515e3224ab
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-24 09:00:43 -0600

    Added ColumnFormat API. Need to add definitions

--- include/tabulate/column_format.hpp
+++ include/tabulate/column_format.hpp
@@ -9,6 +9,55 @@ public:
   Format &width(size_t value);
   Format &height(size_t value);
 
+  // Margin styling
+  Format &margin(size_t value);
+  Format &margin_left(size_t value);
+  Format &margin_right(size_t value);
+  Format &margin_top(size_t value);
+  Format &margin_bottom(size_t value);
+
+  // Padding
+  Format &padding(size_t value);
+  Format &padding_left(size_t value);
+  Format &padding_right(size_t value);
+  Format &padding_top(size_t value);
+  Format &padding_bottom(size_t value);
+
+  // Border
+  Format &border(const std::string &value);
+  Format &border_color(Color value);
+  Format &border_background_color(Color value);
+  Format &border_left(const std::string &value);
+  Format &border_left_color(Color value);
+  Format &border_left_background_color(Color value);
+  Format &border_right(const std::string &value);
+  Format &border_right_color(Color value);
+  Format &border_right_background_color(Color value);
+  Format &border_top(const std::string &value);
+  Format &border_top_color(Color value);
+  Format &border_top_background_color(Color value);
+  Format &border_bottom(const std::string &value);
+  Format &border_bottom_color(Color value);
+  Format &border_bottom_background_color(Color value);
+
+  // Corner
+  Format &corner(const std::string &value);
+  Format &corner_color(Color value);
+  Format &corner_background_color(Color value);
+
+  // Column separator
+  Format &column_separator(const std::string &value);
+  Format &column_separator_color(Color value);
+  Format &column_separator_background_color(Color value);
+
+  // Font styling
+  Format &font_align(FontAlign value);
+  Format &font_style(const std::vector<FontStyle> &style);
+  Format &font_color(Color value);
+  Format &font_background_color(Color value);
+  Format &color(Color value);
+  Format &background_color(Color value);
+
 private:
   std::reference_wrapper<class Column> column_;
 };
--- samples/employees.cpp
+++ samples/employees.cpp
@@ -11,15 +11,18 @@ int main() {
   employees.add_row({"102", "Donald", "Patrick", "Marketing and Operational Logistics Planning"});
   employees.add_row({"103", "Ian", "Jacob", "Engineering"});
 
-  employees.column(3).format()
-    .width(25);
+  employees.column(0).format()
+    .font_align(FontAlign::right);
 
-  employees[0].format()
-    // .font_align(FontAlign::right)
-    .font_style({FontStyle::bold});
+  // employees.column(3).format()
+  //   .width(25);
 
-  employees[1][3].format()
-    .font_align(FontAlign::center);
+  // employees[0].format()
+  //   // .font_align(FontAlign::right)
+  //   .font_style({FontStyle::bold});
+
+  // employees[1][3].format()
+  //   .font_align(FontAlign::center);
 
   // Print the table
   employees.print(std::cout);
