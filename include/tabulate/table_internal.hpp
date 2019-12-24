39dc4222095887a44d4aefc4da1383de90955535
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-24 14:08:09 -0600

    Handled show_border_left/right

--- include/tabulate/table_internal.hpp
+++ include/tabulate/table_internal.hpp
@@ -205,10 +205,12 @@ void Printer::print_row_in_cell(std::ostream &stream, TableInternal &table,
   auto padding_top = format.padding_top_.value();
   auto padding_bottom = format.padding_bottom_.value();
 
-  apply_element_style(stream, format.border_left_color_.value(),
-                      format.border_left_background_color_.value(), {});
-  stream << format.border_left_.value();
-  reset_element_style(stream);
+  if (format.show_border_left_.value()) {
+    apply_element_style(stream, format.border_left_color_.value(),
+                        format.border_left_background_color_.value(), {});
+    stream << format.border_left_.value();
+    reset_element_style(stream);
+  }
 
   apply_element_style(stream, format.font_color_.value(), format.font_background_color_.value(),
                       {});
@@ -283,10 +285,12 @@ void Printer::print_row_in_cell(std::ostream &stream, TableInternal &table,
 
   if (index.second + 1 == num_columns) {
     // Print right border after last column
-    apply_element_style(stream, format.border_right_color_.value(),
-                        format.border_right_background_color_.value(), {});
-    stream << format.border_right_.value();
-    reset_element_style(stream);
+    if (format.show_border_right_.value()) {
+      apply_element_style(stream, format.border_right_color_.value(),
+                          format.border_right_background_color_.value(), {});
+      stream << format.border_right_.value();
+      reset_element_style(stream);
+    }
   }
 }
 
@@ -347,7 +351,7 @@ bool Printer::print_cell_border_bottom(std::ostream &stream, TableInternal &tabl
 
   for (size_t i = 0; i < column_width; ++i) {
     apply_element_style(stream, format.border_bottom_color_.value(),
-                        format.border_left_background_color_.value(), {});
+                        format.border_bottom_background_color_.value(), {});
     stream << border_bottom;
     reset_element_style(stream);
   }
