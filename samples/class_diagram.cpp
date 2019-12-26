eda80e608b80b607077b1123322ec3421e8a6184
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-26 17:35:07 -0600

    Added class diagram sample

---
+++ samples/class_diagram.cpp
@@ -0,0 +1,60 @@
+#include <tabulate/table.hpp>
+using namespace tabulate;
+
+int main() {
+    Table class_diagram;
+    class_diagram.format()
+      .font_style({FontStyle::bold})
+      .font_align(FontAlign::center)
+      .width(60);
+
+    Table animal;
+    animal.add_row({"Animal"});
+
+    Table animal_properties;
+    animal_properties.format().width(20);
+    animal_properties.add_row({"+age: Int"});
+    animal_properties.add_row({"+gender: String"});
+    animal_properties[1].format().hide_border_top();
+
+    Table animal_methods;
+    animal_methods.format().width(20);
+    animal_methods.add_row({"+isMammal()"});
+    animal_methods.add_row({"+mate()"});
+    animal_methods[1].format().hide_border_top();
+
+    animal.add_row({animal_properties});
+    animal.add_row({animal_methods});
+    animal[2].format().hide_border_top();
+
+    class_diagram.add_row({animal});
+
+    class_diagram.add_row({"▲"});
+    class_diagram[1].format().hide_border_top();
+    class_diagram.add_row({"|"});
+    class_diagram[2].format().hide_border_top();
+    class_diagram.add_row({"|"});
+    class_diagram[3].format().hide_border_top();
+
+    Table duck;
+    duck.add_row({"Duck"});
+
+    Table duck_properties;
+    duck_properties.format().width(40);
+    duck_properties.add_row({"+beakColor: String = \"yellow\""});
+
+    Table duck_methods;
+    duck_methods.format().width(40);
+    duck_methods.add_row({"+swim()"});
+    duck_methods.add_row({"+quack()"});
+    duck_methods[1].format().hide_border_top();
+
+    duck.add_row({duck_properties});
+    duck.add_row({duck_methods});
+    duck[2].format().hide_border_top();
+
+    class_diagram.add_row({duck});
+    class_diagram[4].format().hide_border_top();
+
+    std::cout << class_diagram << std::endl;
+}
