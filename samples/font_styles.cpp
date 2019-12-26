c7a839d43b3ddc5b5f97cd059cbea09f3904bd45
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-26 14:58:57 -0600

    Added font styles sample

---
+++ samples/font_styles.cpp
@@ -0,0 +1,36 @@
+#include <tabulate/table.hpp>
+using namespace tabulate;
+
+int main() {
+  Table styled_table;
+  styled_table.add_row({"Bold", "Italic", "Bold & Italic", "Concealed"});
+  styled_table.add_row({"Underline", "Crossed", "Dark", "Bold, Italic & Underlined"});
+
+  styled_table[0][0].format()
+    .font_style({FontStyle::bold});
+
+  styled_table[0][1].format()
+    .font_style({FontStyle::italic});
+
+  styled_table[0][2].format()
+    .font_style({FontStyle::bold, FontStyle::italic});
+
+  styled_table[0][3].format()
+    .font_style({FontStyle::concealed});
+
+  styled_table[1][0].format()
+    .font_style({FontStyle::underline});
+
+  styled_table[1][1].format()
+    .font_style({FontStyle::crossed});
+
+  styled_table[1][2].format()
+    .font_style({FontStyle::dark});
+
+
+  styled_table[1][3].format()
+    .font_style({FontStyle::bold, FontStyle::italic, FontStyle::underline});
+
+  std::cout << styled_table << std::endl;
+
+}
