dd2d82f06a5f7c60bebd13290806dc7ac0839ae6
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-26 17:04:36 -0600

    Word-wrapping sample

---
+++ samples/word_wrapping.cpp
@@ -0,0 +1,15 @@
+#include <tabulate/table.hpp>
+using namespace tabulate;
+
+int main() {
+  Table table;
+
+  table.add_row({
+      "This paragraph contains a veryveryveryveryveryverylong word. The long word will break and word wrap to the next line.",
+      "This paragraph \nhas embedded '\\n' \ncharacters and\n will break\n exactly where\n you want it\n to\n break."});
+
+  table[0][0].format().width(20);
+  table[0][1].format().width(50);
+
+  std::cout << table << std::endl;
+}
