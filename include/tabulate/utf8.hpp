db387bbacc4208553d214aecc002c50c8b2a3892
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-29 16:15:37 -0600

    Cleanup

--- include/tabulate/utf8.hpp
+++ include/tabulate/utf8.hpp
@@ -13,10 +13,12 @@
 namespace tabulate {
 
 #if defined(__unix__) || defined(__unix) || defined(__APPLE__)
-int get_wcswidth(const std::string &string, const std::string &locale, size_t max_column_width) {
+size_t get_wcswidth(const std::string &string, const std::string &locale, size_t max_column_width) {
   if (string.size() == 0)
     return 0;
 
+  // The behavior of wcswidth() depends on the LC_CTYPE category of the current locale.
+  // Set the current locale based on cell properties before computing width
   std::locale::global(std::locale(locale));
 
   // Convert from narrow std::string to wide string
@@ -24,12 +26,12 @@ int get_wcswidth(const std::string &string, const std::string &locale, size_t ma
   std::mbstowcs(wide_string, string.c_str(), string.size());
 
   // Compute display width of wide string
-  auto result = wcswidth(wide_string, max_column_width);
+  int width = wcswidth(wide_string, max_column_width);
   delete[] wide_string;
 
   std::locale::global(std::locale(""));
 
-  return result;
+  return (width > 0) ? width : 0 /* returns 0 if width <= 0 */;
 }
 #endif
 
@@ -40,7 +42,7 @@ size_t get_sequence_length(const std::string &text, const std::string &locale) {
                         [](char c) -> bool { return (c & 0xC0) == 0x80; }));
 #elif defined(__unix__) || defined(__unix) || defined(__APPLE__)
   auto result = get_wcswidth(text, locale, text.size());
-  if (result >= 0)
+  if (result > 0)
     return result;
   else
     return (text.length() - std::count_if(text.begin(), text.end(),
