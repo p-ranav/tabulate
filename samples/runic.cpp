4bb107fe0eec392685056a7578a5db748cc17f5e
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-29 09:39:10 -0600

    Updated runic sample - added colors to  borders and corners

--- samples/runic.cpp
+++ samples/runic.cpp
@@ -31,11 +31,19 @@ int main() {
       .corner_top_right("ᛯ")
       .corner_bottom_left("ᛮ")
       .corner_bottom_right("ᛸ")
+      .corner_top_left_color(Color::cyan)
+      .corner_top_right_color(Color::yellow)
+      .corner_bottom_left_color(Color::green)
+      .corner_bottom_right_color(Color::red)
       // Borders
       .border_top("ᛜ")
       .border_bottom("ᛜ")
       .border_left("ᚿ")
-      .border_right("ᛆ");
+      .border_right("ᛆ")
+      .border_left_color(Color::yellow)
+      .border_right_color(Color::green)
+      .border_top_color(Color::cyan)
+      .border_bottom_color(Color::red);
 
     std::cout << table << std::endl;
 }
