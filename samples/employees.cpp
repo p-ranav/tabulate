365ef3f608dd175e8df95fbbd929a398013e0d1a
Joe Barnier <Joe.Barnier@plexus.com>
2024-02-16 09:57:46 -0600

    Update Employees sample to use RowStream.

--- samples/employees.cpp
+++ samples/employees.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <tabulate/table.hpp>
 using namespace tabulate;
 using Row_t = Table::Row_t;
@@ -8,12 +9,16 @@ int main() {
   Table department;
   department.add_row(Row_t{"Research", "Engineering"});
 
+  RowStream rs;
+  rs << std::setprecision(10);
+
   // Add rows
-  employees.add_row(Row_t{"Emp. ID", "First Name", "Last Name", "Department / Business Unit"});
-  employees.add_row(Row_t{"101", "Donald", "Patrick", "Finance"});
-  employees.add_row(
-      Row_t{"102", "Rachel", "Williams", "Marketing and Operational\nLogistics Planning"});
-  employees.add_row(Row_t{"103", "Ian", "Jacob", department});
+  // clang-format off
+  employees.add_row(Row_t{"Emp. ID", "First Name", "Last Name", "Department / Business Unit", "Pay Rate"});
+  employees.add_row(RowStream{}.copyfmt(rs) << 101 << "Donald" << "Patrick" << "Finance" << 59.61538461);
+  employees.add_row(RowStream{}.copyfmt(rs) << 102 << "Rachel" << "Williams" << "Marketing and Operational\nLogistics Planning" << 34.97067307);
+  employees.add_row(RowStream{}.copyfmt(rs) << 103 << "Ian" << "Jacob" << department << 57.00480769);
+  // clang-format on
 
   employees.column(0)
       .format()
